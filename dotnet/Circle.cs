7db2a5437693da258427e549488f5686d931e770
Shawn <unshorn@gmail.com>
2024-08-16 11:13:20 +1200

    implemented Circle

---
+++ dotnet/Circle.cs
@@ -0,0 +1,9 @@
+namespace Shapes {
+    public class Circle : Interfaces.IShape
+    {
+        public int getArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
